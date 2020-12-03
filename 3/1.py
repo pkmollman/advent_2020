1496b2ccc61e92ee5488fa7edaae4441e7fbc63c
mollman <pkmollman@gmail.com>
2020-12-02 22:00:09 -0800

    day 3

---
+++ 3/1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+matrix   = []
+location = [0,0]
+trees = 0
+big_number = 0
+slopes = [
+    [3,1],
+]
+
+with open('input.txt', 'r') as f:
+    for line in f.readlines():
+        matrix.append(line.strip())
+
+def tree_or_nah(matrix,x,y):
+    r_x = (x % len(matrix[0]))
+    print(r_x, y)
+    print(matrix[y])
+    if matrix[y][r_x] == '#':
+        return True
+    return False
+
+for slope in slopes:
+    while location[1] < len(matrix)-1:
+        location[0] += slope[0]
+        location[1] += slope[1]
+        print(location)
+        if tree_or_nah(matrix,location[0],location[1]):
+            trees += 1
+    if big_number == 0:
+        big_number = trees
+    else:
+        big_number = big_number * trees
+
+print(trees)
+print(big_number)
